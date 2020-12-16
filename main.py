d60d569e480dcff6fdc0602799ec805ed368b882
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-12-16 16:35:10 +0100

    Relative paths

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ from runtime import generateRuntime
 from simulation import runSimulation
 
 #Read the input files
-df_gender = pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Bevolking__kerncijfers_07122020_100024.csv', delimiter = ';')
-df_age = pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Bevolking__kerncijfers_07122020_112736.csv', delimiter = ';')
-df_lifeexpextancy = pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Levensverwachting__geslacht__leeftijd__per_jaar_en_periode_van_vijf_jaren__07122020_120956.csv', delimiter = ';')
-df_maritalstatus = pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Bevolking__geslacht__leeftijd_en_burgerlijke_staat__1_januari_08122020_110015.csv', delimiter = ';')
-df_marriageduration = pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Bestaande_huwelijken_en_partnerschappen__relatieduur__1_januari_08122020_121148.csv', delimiter = ';')
-df_employmentstatus = pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Arbeidsdeelname__kerncijfers__08122020_130106.csv', delimiter = ';')
-df_incomedistribution = pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Inkomen_van_personen__inkomensklassen__persoonskenmerken_09122020_094158.csv', delimiter = ';')
-df_marriage = pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Huwen_en_huwelijksontbinding__geslacht__leeftijd__31_december___regio_11122020_100116.csv', delimiter = ';')
-df_marriage2 = pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Bevolking__geslacht__leeftijd_en_burgerlijke_staat__1_januari_11122020_105220.csv', delimiter = ';')
-df_withchildren =  pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Particuliere_huishoudens_naar_samenstelling_en_grootte__1_januari_14122020_114929.csv', delimiter = ';')
-df_nrchildren = pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Huishoudens__kindertal__leeftijdsklasse_kind__regio__1_januari_14122020_114332.csv', delimiter = ';')
-df_birthage =  pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/Levend_geboren_kinderen__migratieachtergrond_moeder_en_leeftijd_moeder_14122020_112329.csv', delimiter = ';')
-df_capital = pd.read_csv('C:/Users/Raymo/OneDrive/Documents/GitHub/simulation-abm/Input_2/vermogensklassen.csv', delimiter = ';')
+df_gender = pd.read_csv('./Input_2/Bevolking__kerncijfers_07122020_100024.csv', delimiter = ';')
+df_age = pd.read_csv('./Input_2/Bevolking__kerncijfers_07122020_112736.csv', delimiter = ';')
+df_lifeexpextancy = pd.read_csv('./Input_2/Levensverwachting__geslacht__leeftijd__per_jaar_en_periode_van_vijf_jaren__07122020_120956.csv', delimiter = ';')
+df_maritalstatus = pd.read_csv('./Input_2/Bevolking__geslacht__leeftijd_en_burgerlijke_staat__1_januari_08122020_110015.csv', delimiter = ';')
+df_marriageduration = pd.read_csv('./Input_2/Bestaande_huwelijken_en_partnerschappen__relatieduur__1_januari_08122020_121148.csv', delimiter = ';')
+df_employmentstatus = pd.read_csv('./Input_2/Arbeidsdeelname__kerncijfers__08122020_130106.csv', delimiter = ';')
+df_incomedistribution = pd.read_csv('./Input_2/Inkomen_van_personen__inkomensklassen__persoonskenmerken_09122020_094158.csv', delimiter = ';')
+df_marriage = pd.read_csv('./Input_2/Huwen_en_huwelijksontbinding__geslacht__leeftijd__31_december___regio_11122020_100116.csv', delimiter = ';')
+df_marriage2 = pd.read_csv('./Input_2/Bevolking__geslacht__leeftijd_en_burgerlijke_staat__1_januari_11122020_105220.csv', delimiter = ';')
+df_withchildren =  pd.read_csv('./Input_2/Particuliere_huishoudens_naar_samenstelling_en_grootte__1_januari_14122020_114929.csv', delimiter = ';')
+df_nrchildren = pd.read_csv('./Input_2/Huishoudens__kindertal__leeftijdsklasse_kind__regio__1_januari_14122020_114332.csv', delimiter = ';')
+df_birthage =  pd.read_csv('./Input_2/Levend_geboren_kinderen__migratieachtergrond_moeder_en_leeftijd_moeder_14122020_112329.csv', delimiter = ';')
+df_capital = pd.read_csv('./Input_2/vermogensklassen.csv', delimiter = ';')
 
 print(df_incomedistribution.head())
 

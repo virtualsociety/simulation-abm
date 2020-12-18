870ac7ce7a790b12cf0bb1cf505ec467e3fec0aa
RaymondHoogendoorn-bit <69454677+RaymondHoogendoorn-bit@users.noreply.github.com>
2020-12-18 09:41:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,22 +21,13 @@ df_lifeexpextancy = pd.read_csv('./Input_2/Levensverwachting__geslacht__leeftijd
 df_maritalstatus = pd.read_csv('./Input_2/Bevolking__geslacht__leeftijd_en_burgerlijke_staat__1_januari_08122020_110015.csv', delimiter = ';')
 df_marriageduration = pd.read_csv('./Input_2/Bestaande_huwelijken_en_partnerschappen__relatieduur__1_januari_08122020_121148.csv', delimiter = ';')
 df_employmentstatus = pd.read_csv('./Input_2/Arbeidsdeelname__kerncijfers__08122020_130106.csv', delimiter = ';')
-<<<<<<< HEAD
 df_income = pd.read_csv('./Input_2/Inkomen_van_personen__inkomensklassen__persoonskenmerken_09122020_094158.csv', delimiter = ';')
-=======
-df_incomedistribution = pd.read_csv('./Input_2/Inkomen_van_personen__inkomensklassen__persoonskenmerken_09122020_094158.csv', delimiter = ';')
->>>>>>> d60d569e480dcff6fdc0602799ec805ed368b882
 df_marriage = pd.read_csv('./Input_2/Huwen_en_huwelijksontbinding__geslacht__leeftijd__31_december___regio_11122020_100116.csv', delimiter = ';')
 df_marriage2 = pd.read_csv('./Input_2/Bevolking__geslacht__leeftijd_en_burgerlijke_staat__1_januari_11122020_105220.csv', delimiter = ';')
 df_withchildren =  pd.read_csv('./Input_2/Particuliere_huishoudens_naar_samenstelling_en_grootte__1_januari_14122020_114929.csv', delimiter = ';')
 df_nrchildren = pd.read_csv('./Input_2/Huishoudens__kindertal__leeftijdsklasse_kind__regio__1_januari_14122020_114332.csv', delimiter = ';')
 df_birthage =  pd.read_csv('./Input_2/Levend_geboren_kinderen__migratieachtergrond_moeder_en_leeftijd_moeder_14122020_112329.csv', delimiter = ';')
 df_capital = pd.read_csv('./Input_2/vermogensklassen.csv', delimiter = ';')
-<<<<<<< HEAD
-=======
-
-print(df_incomedistribution.head())
->>>>>>> d60d569e480dcff6fdc0602799ec805ed368b882
 
 #Initialize main variables
 start_date = '2011-01-01' #Set the start date of the simulation
